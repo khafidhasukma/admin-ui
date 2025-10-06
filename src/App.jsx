4284dc6992390325cd7287aa5c4477cebc5044e4
Khafidha Sukma <khafidhasukma@gmail.com>
2025-10-06 20:01:42 +0700

    feat: implement login form layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="bg-neutral-800 min-h-screen flex flex-col items-center justify-center text-center">
-      <div className="flex items-center space-x-8 mb-12">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="size-24" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="size-24 animate-spin" alt="React logo" style={{ animationDuration: "10s" }} />
-        </a>
+    <div className="container px-5 bg-gray-100 min-h-screen flex justify-center items-center">
+      <div className="grid md:grid-cols-2 max-w-2xl lg:max-w-3xl w-full rounded-lg overflow-hidden shadow">
+        <div className="col max-md:hidden bg-gray-200 w-full flex justify-center items-center">
+          <h1 className="text-4xl font-semibold text-gray-500">600 x 500</h1>
+        </div>
+        <div className="col bg-white w-full px-6 pt-5 pb-12">
+          <h1 className="text-2xl text-center font-semibold">Login</h1>
+          <div className="space-y-4">
+            {/* Email */}
+            <div className="space-y-1">
+              <label htmlFor="email" className="block font-semibold text-sm">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="border border-gray-300 focus:outline-blue-600 rounded-md p-2 w-full"
+                placeholder="Masukkan email"
+              />
+            </div>
+            {/* Password */}
+            <div className="space-y-1">
+              <label htmlFor="password" className="block font-semibold text-sm">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="border border-gray-300 focus:outline-blue-600 rounded-md p-2 w-full"
+                placeholder="Masukkan password"
+              />
+            </div>
+
+            {/* Button Submit */}
+            <button
+              type="submit"
+              className="bg-blue-500 text-white rounded-md p-2 w-full cursor-pointer hover:bg-blue-600 transition duration-300">
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-      <h1 className="text-8xl font-bold text-white mb-16">Vite + React</h1>
-      <div className="text-sm space-y-6 mb-6">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg">
-          count is {count}
-        </button>
-        <p className="text-neutral-400">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-neutral-500">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
