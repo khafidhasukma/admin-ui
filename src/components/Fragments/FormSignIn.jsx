29a4339fb17efd269f38b8786ee341e56e4473b3
Khafidha Sukma <khafidhasukma@gmail.com>
2025-12-08 23:39:22 +0700

    react router dom

--- src/components/Fragments/FormSignIn.jsx
+++ src/components/Fragments/FormSignIn.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LabeledInput from "../Elements/LabeledInput";
 import CheckBox from "../Elements/CheckBox";
 import Button from "../Elements/Button";
+import { Link } from "react-router-dom";
 
 function FormSignIn() {
   return (
@@ -46,9 +47,9 @@ function FormSignIn() {
       {/* sign in with google end */}
       {/* link start */}
       <div className="flex justify-center">
-        <a href="#" className="text-primary text-sm font-bold">
+        <Link to="/register" className="text-primary text-sm font-bold">
           Create an account
-        </a>
+        </Link>
       </div>
       {/* link end */}
     </>
